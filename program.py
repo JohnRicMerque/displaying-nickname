cb7dd869a014317d123901e176d276562faf7dcc
John Ric Merque <johnricmer@gmail.com>
2022-10-22 16:46:52 +0800

    added text colors

--- program.py
+++ program.py
@@ -2,7 +2,7 @@
 # Create a program that will print your nickname using only asterisk character (*)
 # Example:
 
-def makeLetterAsteriskList (letter):
+def makeLetterAsteriskList (letter, color):
     charList = []
     for row in range(5):    
         innerList = []
@@ -10,23 +10,32 @@ def makeLetterAsteriskList (letter):
 
             # condition reference
             conditions = {
-            "J" : (col == 4 or row == 4  or (row == 3 and col == 0)),
-            "O" : ((row == 0 or row==4) and (col != 0 and col != 4)) or ((col == 0 or col==4) and (row != 0 and row != 4)),
-            "H" : col == 0 or col == 4 or row == 2,
-            "N" : col == 0 or col == 4 or (row == col)}
+                "J" : (col == 4 or row == 4  or (row == 3 and col == 0)),
+                "O" : ((row == 0 or row==4) and (col != 0 and col != 4)) or ((col == 0 or col==4) and (row != 0 and row != 4)),
+                "H" : col == 0 or col == 4 or row == 2,
+                "N" : col == 0 or col == 4 or (row == col)
+            }
+
+            # colors reference
+            colors = {
+                "green" : "\033[1;32;40m",
+                "red" : "\033[1;31;40m",
+                "yellow" : "\033[1;33;40m",
+                "white" : "\033[1;37;40m"
+            }
 
             if conditions[letter]:
-                innerList.append("*")
+                innerList.append(f"{colors[color]}*")
             else:
                 innerList.append(" ")
         charList.append(innerList)
     return charList
 
 # make asterisk lists for each character
-charJ = makeLetterAsteriskList("J")
-charO = makeLetterAsteriskList("O")
-charH = makeLetterAsteriskList("H")
-charN = makeLetterAsteriskList("N")
+charJ = makeLetterAsteriskList("J", "green")
+charO = makeLetterAsteriskList("O", "red")
+charH = makeLetterAsteriskList("H", "yellow")
+charN = makeLetterAsteriskList("N", "white")
 
 # print loop for each characters list
 for row in range(5):
